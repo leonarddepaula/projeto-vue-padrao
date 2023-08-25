0a4c6125cff40e787c668c422137f130a0f7f9ea
Leonardo De Paula <92495561+leonarddepaula@users.noreply.github.com>
2023-08-25 16:20:47 -0300

    ambientes de desenvolvimento

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,22 @@
 <template>
-  <h1>componente olá nundo</h1>
+  <div>
+
+    <h1>componente olá nundo {{ variavel }}</h1>
+    <p>Link da API: {{ url_api }}</p>
+    <p>Modo: {{ modo }}</p>
+    
+  </div>
 </template>
 
 <script>
 export default {
+  data: () =>{
+    return {
+      variavel : process.env.VUE_APP_MINHA_VARIAVEL_DE_AMBIENTE,
+      url_api: process.env.VUE_APP_API_URL,
+      modo: process.env.VUE_APP_MODO
+    }
+  },
   name: 'HelloWorld',
   
 }
